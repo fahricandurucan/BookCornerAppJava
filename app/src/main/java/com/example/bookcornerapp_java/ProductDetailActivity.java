74c5d7829019c677c11f5e654342d6b4ade30fda
Simge Özgündöndü <simgeozgndndu@gmail.com>
2023-12-26 14:02:32 +0300

    updated

--- app/src/main/java/com/example/bookcornerapp_java/ProductDetailActivity.java
+++ app/src/main/java/com/example/bookcornerapp_java/ProductDetailActivity.java
@@ -2,13 +2,24 @@ package com.example.bookcornerapp_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ProductDetailActivity extends AppCompatActivity {
 
+    ImageView btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail);
+        btnBack=findViewById(R.id.backBtn);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
